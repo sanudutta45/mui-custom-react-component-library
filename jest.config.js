21f309baf1a8e38a95d52208c05cba579cbb558e
Sanu <sanudutta45@gmail.com>
2023-08-05 23:37:51 +0530

    CUMUI-23: jest config

--- jest.config.js
+++ jest.config.js
@@ -136,8 +136,10 @@ const config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
-
+  setupFilesAfterEnv: [
+    "@testing-library/jest-dom/extend-expect",
+    "@testing-library/user-event",
+  ],
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
 
